62dffe27b21a299b2c6faef72cbf18afb2b99fb8
kurumaebi <knight7446@gmail.com>
2021-05-17 12:45:47 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-          <h1>Hello, world!</h1>
-      </div>
-    );
-  }
+// JSXで書いた場合
+class App extends Component
+{
+	render()
+	{
+		// Classと入れ込み
+			// const greeting = "Hi";
+			// const dom = <div className="foo">{greeting}</div>
+		// 代入してreturnもできる
+			// const dom = <h1>Hello, world!</h1>;
+			// return dom;
+		return (
+			<React.Fragment>
+				<label htmlFor="bar">bar</label>
+				<input type="text" onChange={() => {console.log("I am clicked!")}} />
+			</React.Fragment>
+		)
+	}
 }
 
+// javascriptで書いた場合
+// class App extends Component
+// {
+// 	render() {
+// 		return React.createElement
+// 		(
+// 			"div",
+// 			null,
+// 			"Hello"
+// 		);
+// 	}
+// }
 export default App;
