959658a4c3e48ee715890720d092b873b1f1c057
kurumaebi <knight7446@gmail.com>
2021-05-17 14:07:46 +0900

    compornent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 
-// JSXで書いた場合
-class App extends Component
-{
-	render()
-	{
-		// Classと入れ込み
-			// const greeting = "Hi";
-			// const dom = <div className="foo">{greeting}</div>
-		// 代入してreturnもできる
-			// const dom = <h1>Hello, world!</h1>;
-			// return dom;
-		return (
-			<React.Fragment>
-				<label htmlFor="bar">bar</label>
-				<input type="text" onChange={() => {console.log("I am clicked!")}} />
-			</React.Fragment>
-		)
-	}
+// classコンポーネント
+// class App extends Component
+// {
+// 	render()
+// 	{
+// 		// Classと入れ込み
+// 			// const greeting = "Hi";
+// 			// const dom = <div className="foo">{greeting}</div>
+// 		// 代入してreturnもできる
+// 			// const dom = <h1>Hello, world!</h1>;
+// 			// return dom;
+// 		return (
+// 			<React.Fragment>
+// 				<label htmlFor="bar">bar</label>
+// 				<input type="text" onChange={() => {console.log("I am clicked!")}} />
+// 			</React.Fragment>
+// 		)
+// 	}
+// }
+// 関数コンポーネント
+const App = () => {
+	return (
+	<div>
+		<Cat />
+		<Cat />
+		<Cat />
+		<Cat />
+		<Cat />
+	</div>
+	)
+}
+
+const Cat = () => {
+	return <div>MEow!</div>
 }
 
 // javascriptで書いた場合
